9d165dd70e61d897f1d09b86e015befe1fdbf0d0
arnaud <arnaud@idelub.com>
2020-08-21 11:41:24 +0200

    Fixing ship-item's path

--- src/app/ships/ships.module.ts
+++ src/app/ships/ships.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ShipsComponent } from './ships.component';
 import { ShipsListComponent } from './ships-list/ships-list.component';
 import { ShipDetailsComponent } from './ship-details/ship-details.component';
-import { ShipItemComponent } from './ship-list/ship-item/ship-item.component';
 
 
 
 @NgModule({
-  declarations: [ShipsComponent, ShipsListComponent, ShipDetailsComponent, ShipItemComponent],
+  declarations: [ShipsComponent, ShipsListComponent, ShipDetailsComponent,],
   imports: [
     CommonModule
   ]
