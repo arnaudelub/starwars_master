eece685f8ce62c230cfd37b98ba1d9907ba08e49
arnaud <arnaud@idelub.com>
2020-08-22 11:29:43 +0200

    ADding search bar and account icon

--- src/app/ships/ships.component.ts
+++ src/app/ships/ships.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ShipsService } from './ships.service';
 import { Observable } from 'rxjs';
 import { swapiResponse } from '../models/starship';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-ships',
   templateUrl: './ships.component.html',
@@ -10,10 +11,15 @@ import { swapiResponse } from '../models/starship';
 })
 export class ShipsComponent implements OnInit {
   starships$: Observable<swapiResponse>
-  constructor(private shipsService: ShipsService) { }
+  constructor(private shipsService: ShipsService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.starships$ = this.shipsService.getStarship();
+  }
+
+  isShipListRoute() {
+    return this.router.isActive('ships', true);
   }
 
 }
